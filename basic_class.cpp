adcbee5256acf31037e8af9ab819cd7eabfb13a3
Rogerlv51 <949608370@qq.com>
2024-01-02 13:43:46 +0800

    Feature: 添加关于移动构造函数的说明

--- basic_class.cpp
+++ basic_class.cpp
@@ -1,8 +1,67 @@
 /*                    类的基础知识，包括继承多态等功能          */
 
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+struct Vector
+{
+  size_t m_size;
+  int *m_data;
+
+  Vector(size_t n)
+  {
+    m_size = n;
+    m_data = (int *)malloc(n * sizeof(int));
+  }
+
+  Vector(const Vector &other){
+    m_size = other.m_size;
+    m_data = (int *)malloc(m_size * sizeof(int));
+    // 注意要使用内存拷贝，而不是直接赋值 m_data = other.m_data!!!
+    memcpy(m_data, other.m_data, other.m_size * sizeof(int));
+  }
+
+  Vector &operator=(const Vector &other){
+    this->~Vector();  // 先销毁当前自身的空间
+    new(this) Vector(other);  // 再调用拷贝构造函数
+    return *this;
+
+    // 或者像下面这种写法
+    // m_size = other.m_size;
+    // m_data = (int *)malloc(m_size * sizeof(int));
+    // // 注意要使用内存拷贝，而不是直接赋值 m_data = other.m_data!!!
+    // memcpy(m_data, other.m_data, other.m_size * sizeof(int));
+    // return *this;
+  }
+
+  // 移动构造
+  Vector(Vector &&other) : m_size(other.m_size), m_data(other.m_data){
+    other.m_size = 0;
+    other.m_data = nullptr;
+  }
+
+  ~Vector()
+  {
+    free(m_data);
+  }
+
+  size_t size()
+  {
+    return m_size;
+  }
+
+  void resize(size_t n)
+  {
+    m_size = n;
+    m_data = (int *)realloc(m_data, n * sizeof(int));
+  }
+
+  int &operator[](size_t index)
+  {
+    return m_data[index];
+  }
+};
 
 class Teacher{
 public:
@@ -88,4 +147,24 @@ int main(){
     C.justPrint("wuwuwu");    // 可见成功调用子类多态函数 
     A.justadd();
     return 0;
+
+    // 对于一个类来说我们知道编译器通常会自动生成默认的构造函数、拷贝构造、赋值构造、移动构造、析构函数等
+    // 因此当我们自己创建一个类的时候应该手动实现上述这些函数，又或者是直接用C11特性default来声明默认，或者delete删除默认
+    // 下面我们阐述一个例子来说明什么是移动构造？对于stl标准模版库来说直接使用std::move()函数就可以实现移动构造
+    vector<int> v1(2);
+    v1[0] = 1;
+    v1[1] = 5;
+    vector<int> v2(3);
+    v2[0] = 2;
+    v2[1] = 3;
+    v2[2] = 4;
+
+    // 如果使用v1=v2即赋值构造，那么v1的大小会变成3，同时不会释放v2的空间，这样时间复杂度是O(n)
+    v1 = move(v2);  // 然而使用move函数即移动的意思，把v2的空间移动给v1，同时v2的空间会被释放，这样时间复杂度是O(1)
+    cout << v1.size() << endl;  // 输出3
+    cout << v2.size() << endl;  // 输出0
+    
+    for(size_t j = 0; j < v1.size(); j++){
+        cout << v1[j] << endl;
+    }
 }
